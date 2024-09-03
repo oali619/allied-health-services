015917fb988193e9ad3175522901ce005ec4fb09
oali619 <obsaahmedali@gmail.com>
2024-09-03 00:58:37 -0500

    update image import

--- app/images/index.js
+++ app/images/index.js
@@ -2,6 +2,6 @@ import apartmentJPG from './apartment.jpg';
 import apartmentJPEG from './apartment.jpeg';
 import armhsImg from './armhs-image.jpeg';
 import hssImage from './hss-image.jpeg';
-import dhsImage from './245d-image.jpeg';
+import dhsImage from './245D-image.jpeg';
 
 export { apartmentJPG, apartmentJPEG, armhsImg, hssImage, dhsImage };
