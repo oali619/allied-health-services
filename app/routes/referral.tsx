85b5e3edf41bab9c8fa46cd2f6d355034aaf8c34
oali619 <obsaahmedali@gmail.com>
2024-09-26 14:28:09 -0500

    remove z-index

--- app/routes/referral.tsx
+++ app/routes/referral.tsx
@@ -111,7 +111,7 @@ export default function Referral() {
 								<Label className='block text-sm font-medium leading-6 text-gray-900'>
 									Referrer Type
 								</Label>
-								<div className='relative mt-2 z-[-1]'>
+								<div className='relative mt-2'>
 									<ListboxButton className='w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6'>
 										<span className='flex items-center'>
 											<span className='ml-3 block truncate'>
@@ -287,7 +287,7 @@ export default function Referral() {
 									<Label className='block text-sm font-medium leading-6 text-gray-900'>
 										Insurance
 									</Label>
-									<div className='relative mt-2 z-[-1]'>
+									<div className='relative mt-2'>
 										<ListboxButton className='relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6'>
 											<span className='flex items-center'>
 												<span className='ml-3 block truncate'>
@@ -337,7 +337,7 @@ export default function Referral() {
 									<Label className='block text-sm font-medium leading-6 text-gray-900'>
 										Waiver Type
 									</Label>
-									<div className='relative mt-2 z-[-1]'>
+									<div className='relative mt-2'>
 										<ListboxButton className='relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6'>
 											<span className='flex items-center'>
 												<span className='ml-3 block truncate'>
